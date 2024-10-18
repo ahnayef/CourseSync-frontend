cb831a97ebf0fbb878a209ac2aa05c2fa4790178
AHNayef <ahnayef@outlook.com>
2024-10-19 00:59:27 +0600

    Configure tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>CourseSync</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-green-500 text-xl">CourseSync</Text>
+      <Text className="">Will change the game!</Text>
     </View>
   );
 }
