82ed1f6d5f80600177372ceab56b3166cb978350
AHNayef <ahnayef@outlook.com>
2024-10-19 00:04:50 +0600

    Init Project

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -8,10 +8,10 @@ import { useColorScheme } from 'react-native';
 import { Colors } from '@/constants/Colors';
 
 export function useThemeColor(
-  props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+  props: { light?: string; },
+  colorName: keyof typeof Colors.light
 ) {
-  const theme = useColorScheme() ?? 'light';
+  const theme = 'light';
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
