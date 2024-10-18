9a87797cb3dd18da3d41c61fb91d1876dc9afe7f
AHNayef <ahnayef@outlook.com>
2024-10-19 01:01:46 +0600

    Configure prettier for formatting tailwind class

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-green-500 text-xl">CourseSync</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl text-green-500">CourseSync</Text>
       <Text className="">Will change the game!</Text>
     </View>
   );
